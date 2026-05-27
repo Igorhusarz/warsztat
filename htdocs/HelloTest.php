5c22913f3e486219a624f6c897159f98c3a4ca17
Ruti-afk <aleksmuravskiy26@gmail.com>
2026-05-27 11:14:45 +0200

    testttt

---
+++ htdocs/HelloTest.php
@@ -0,0 +1,15 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once("unit.php");
+
+final class HelloTest extends TestCase
+{
+    
+    public function testHello(): void
+    {
+        $input = "olek";
+        $result = hello($input);
+        $this->assertSame($result, "hello olek");
+    }
+}
